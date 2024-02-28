6c8dd4309cc6a306ea208e4a2ce6dc091ad4073e
Napruz <napruzxiaomi@gmail.com>
2024-02-28 10:08:28 +0300

    работа над странией content

--- src/pages/CoursesContent.vue
+++ src/pages/CoursesContent.vue
@@ -1,28 +1,35 @@
 <template>
-  <q-breadcrumbs>
-    <q-breadcrumbs-el label="Home" />
-    <q-breadcrumbs-el label="Components" />
-    <q-breadcrumbs-el label="Breadcrumbs" />
-  </q-breadcrumbs>
+  //Хлебные крошки
+  <div class="q-ml-xl q-mt-xl q-gutter-sm">
+    <q-breadcrumbs
+      class="q-mt-lg g-ml-lg"
+      style="active-color: secondary"
+    >
+      <q-breadcrumbs-el label="Учебный центр" />
+      <q-breadcrumbs-el label="Каталог курсов" />
+      <q-breadcrumbs-el label="Содержание" />
+    </q-breadcrumbs>
+  </div>
 
-  <div class="row justify-around">
-    <div class="q-mt-lg q-ml-lg row text-h3 text-weight-bold">
-      <p class="text-primary text-uppercase">
+  //Содержание и логотип компании
+  <div class="row justify-between">
+    <div class="q-mt-lg q-ml-lg q-pl-lg row text-h3 text-weight-bold text-capitalize">
+      <p class="text-primary ">
         Содержание
       </p>
     </div>
 
-    <div class="q-mt-lg q-mr-lg row">
-      <q-avatar>
-        <q-img
-          class="rounded-borders"
-          src="https://cdn.quasar.dev/img/parallax2.jpg"
-          alt="Логотип компании"
-        />
-      </q-avatar>
+    <div class="q-mt-lg q-mr-lg q-pr-lg row">
+      <q-img
+        class="rounded-borders"
+        src="https://cdn.quasar.dev/img/parallax2.jpg"
+        alt="Логотип компании"
+        style="height: 50px; width: 120px"
+      />
     </div>
   </div>
 
+  //Карточки разделов курсов
   <div class="q-pa-md row items-start q-gutter-md row justify-around wrap content-center">
     <q-card
       class="my-card"
@@ -30,24 +37,24 @@
       bordered
     >
       <q-card-section horizontal>
-        <q-card-section class="q-pt-xs">
-          <div class="text-h5 q-mt-sm q-mb-xs text-uppercase">Профессиональные навыки</div>
+        <q-card-section class="q-pt-lg">
+          <div class="text-h5 q-mt-sm q-mb-xs text-primary">
+            Профессиональные навыки
+          </div>
         </q-card-section>
         <q-card-section class="col-5 flex flex-center">
           <q-img
-            class="rounded-borders"
+            class="rounded-borders q-pr-lg q-pb-lg"
             src="https://cdn.quasar.dev/img/parallax2.jpg"
             alt="Люди в офисе"
           />
         </q-card-section>
       </q-card-section>
 
-      <q-separator />
-
       <q-card-actions>
         <q-btn
           size="15px"
-          class="q-px-xl q-py-xs"
+          class="q-px-xl q-py-xs q-ml-lg q-mb-md"
           color="primary"
           label="Открыть"
           to="#"
@@ -61,8 +68,8 @@
       bordered
     >
       <q-card-section horizontal>
-        <q-card-section class="q-pt-xs">
-          <div class="text-h5 q-mt-sm q-mb-xs text-uppercase">
+        <q-card-section class="q-pt-lg">
+          <div class="text-h5 q-mt-sm q-mb-xs text-primary ">
             Вводные курсы (инструктажи)
           </div>
         </q-card-section>
@@ -76,12 +83,10 @@
         </q-card-section>
       </q-card-section>
 
-      <q-separator />
-
       <q-card-actions>
         <q-btn
           size="15px"
-          class="q-px-xl q-py-xs"
+          class="q-px-xl q-py-xs q-ml-lg q-mb-md"
           color="primary"
           label="Открыть"
           to="#"
@@ -95,9 +100,9 @@
       bordered
     >
       <q-card-section horizontal>
-        <q-card-section class="q-pt-xs">
-          <div class="text-h5 q-mt-sm q-mb-xs text-uppercase">
-            ЛИЧНАЯ ЭФФЕКТИВНОСТЬ И РАЗВИТИЕ
+        <q-card-section class="q-pt-lg">
+          <div class="text-h5 q-mt-sm q-mb-xs text-primary ">
+            Личная эффективность и развитие
           </div>
         </q-card-section>
 
@@ -110,12 +115,10 @@
         </q-card-section>
       </q-card-section>
 
-      <q-separator />
-
       <q-card-actions>
         <q-btn
           size="15px"
-          class="q-px-xl q-py-xs"
+          class="q-px-xl q-py-xs q-ml-lg q-mb-md"
           color="primary"
           label="Открыть"
         />
@@ -128,8 +131,8 @@
       bordered
     >
       <q-card-section horizontal>
-        <q-card-section class="q-pt-xs">
-          <div class="text-h5 q-mt-sm q-mb-xs text-uppercase">
+        <q-card-section class="q-pt-lg">
+          <div class="text-h5 q-mt-sm q-mb-xs text-primary ">
             Управление эффективности деятельности (УЭД)
           </div>
         </q-card-section>
@@ -143,12 +146,10 @@
         </q-card-section>
       </q-card-section>
 
-      <q-separator />
-
       <q-card-actions>
         <q-btn
           size="15px"
-          class="q-px-xl q-py-xs"
+          class="q-px-xl q-py-xs q-ml-lg q-mb-md"
           color="primary"
           label="Открыть"
         />
@@ -163,5 +164,5 @@
 <style lang="sass" scoped>
 .my-card
   width: 100%
-  max-width: 550px
+  max-width: 500px
 </style>
