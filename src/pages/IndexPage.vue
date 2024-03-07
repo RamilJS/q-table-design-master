617d508b78685ca84b6d9ca7d8693480b258387b
Napruz <napruzxiaomi@gmail.com>
2024-03-07 13:45:40 +0300

    delete main page content

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -8,38 +8,8 @@
       label="Каталог курсов"
       :to="{ path: 'content' }"
     />
-    <UserTable
-      v-model:selected="selected"
-      :users="users"
-    />
   </q-page>
 </template>
 
 <script setup>
-import UserTable from 'src/models/user/table/UserTable.vue'
-import statuses from 'src/models/user/table/statuses'
-import { ref } from 'vue'
-
-const selected = ref()
-
-const users = [
-  {
-    id: 27346,
-    name: 'Luke',
-    imgSrc: 'https://picsum.photos/200/200',
-    status: statuses.inProgress
-  },
-  {
-    id: 27396,
-    name: 'Lily',
-    imgSrc: 'https://picsum.photos/210/210',
-    status: statuses.notRegistered
-  },
-  {
-    id: 2796,
-    name: 'Jonny',
-    imgSrc: 'https://picsum.photos/210/210',
-    status: statuses.inProgress
-  }
-]
 </script>
