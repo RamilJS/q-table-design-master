8517c9868f2987fa597112ab796b7650c5f7216f
Napruz <napruzxiaomi@gmail.com>
2024-03-12 12:11:50 +0300

    отключил компонент банер от страницы с разделом курсов

--- src/pages/CourseCatalogItems.vue
+++ src/pages/CourseCatalogItems.vue
@@ -65,7 +65,49 @@
                     />
                   </div>
                 </div>
-                <CourseCatalogBanner />
+                <div class="q-mb-md course-catalog-banner">
+                  <q-banner
+                    class="bg-grey-4 banner"
+                    rounded
+                  >
+                    <p class="courses-purpose text-secondary text-h6 q-mb-sm text-uppercase text-bold">
+                      Цель курса?
+                      <q-icon
+                        name="adjust"
+                        style="margin-bottom: 3px;"
+                      />
+                    </p>
+                    <p
+                      class="courses-purpose-description text-primary q-mb-md ellipsis-2-lines"
+                      style="max-width: 700px;"
+                    >
+                      сформировать практические занания о понятии лизинга, его преимуществах
+                      по сравнению с другими способами приобретения ТС и преимуществах работы с ВТБ Лизинг
+                    </p>
+                    <p class="courses-time text-secondary text-h6 q-mb-sm text-uppercase text-bold">
+                      Время прохождения
+                      <q-icon
+                        name="schedule"
+                        style="margin-bottom: 3px;"
+                      />
+                    </p>
+                    <p
+                      class="courses-time-item text-primary q-mb-sm text-lowercase"
+                      style="max-width: 700px;"
+                    >
+                      60 минут
+                    </p>
+                    <template
+                      v-slot:action
+                    >
+                      <q-btn
+                        flat
+                        color="primary"
+                        label="Закрыть"
+                      />
+                    </template>
+                  </q-banner>
+                </div>
               </li>
               <li class="list-item">
                 <div class="row justify-between">
@@ -328,12 +370,7 @@
 </template>
 
 <script>
-import CourseCatalogBanner from 'src/components/CourseBanner.vue'
-export default {
-  components: {
-    CourseCatalogBanner
-  }
-}
+
 </script>
 
 <style scoped>
